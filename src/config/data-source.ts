99ef9ab84977a2d82fb799a48d65fdc16fd3be77
hashimrmzn <hashimramzan246@gmail.com>
2025-10-13 04:13:48 +0500

    completed middleware

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -6,7 +6,12 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, MODE } = process.env;
-
+console.log("Loaded DB Config:", {
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  name: DB_NAME,
+});
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: DB_HOST || "localhost",
@@ -16,6 +21,6 @@ export const AppDataSource = new DataSource({
   database: DB_NAME || "survey_app",
  synchronize: false,
   logging: false,
-  entities: ["src/entities/**/*.ts"],
-  migrations: ["src/migrations/**/*.ts"],
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
 });
